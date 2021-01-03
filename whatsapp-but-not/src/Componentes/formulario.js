d7b39082633327d63536f3612ec033b169953f77
Jose Modolo <josenetozxc@yahoo.com>
2021-01-03 20:37:56 -0300

    projeto whats

--- whatsapp-but-not/src/Componentes/formulario.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function enviarMensagem (){
-let mensagemUser = document.getElementsByClassName("mensagem").value
-document.getElementsByClassName.innerHTML += mensagemUser
-
-}
